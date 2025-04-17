792b964651095c7c488295d55dccda3cbfa6fd24
Barabasz0222 <lucasbarabasz@hotmail.com>
2025-04-16 21:29:31 -0300

    Configurações iniciais

---
+++ src/components/InicialMenuComponent/index.tsx
@@ -0,0 +1,13 @@
+// src/components/Home.tsx
+import React from "react";
+
+
+const InicialMenuComponent = () => {
+  return (
+    <>
+<h1>oi</h1>
+    </>
+  );
+};
+
+export default InicialMenuComponent;
---
+++ src/components/ModelPage/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./style.module.css";
+import Link from "next/link";
+
+interface ModelPageProps {
+  nomePagina: string;
+  content: React.ReactNode;
+}
+
+export default function ModelPage({ nomePagina, content }: ModelPageProps) {
+  return (
+    <main className={styles.main}>
+      <div className={styles.containerContent}>
+        <div className={styles.containerButtonReturn}>
+          <h1 className={styles.pageTitle}>{nomePagina}</h1>
+        </div>
+
+        <div className={styles.content}>{content}</div>
+      </div>
+    </main>
+  );
+}
